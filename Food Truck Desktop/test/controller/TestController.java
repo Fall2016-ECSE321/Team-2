7d07ee75f556c782c33ce101631ce1d432e29809
HankyZ <yuhuan01@hotmail.com>
2016-10-19 21:30:40 -0400

    Updated Iteration1
    Created createMenuItem, TestMenuItem
    Updated TestPersistence, TestController 
    
    BUG with TESTCONTROLLER xml file that only "shows" 1 test result, even though it can load the invisible values o.o
    I am testing all 3 staff, supply, equip together
    1. if createEquip gets commented out, xml file will show staff info
    2. if createStaff gets commented out, xml file will show Equip info
    3. if equip staff both get commented out, xml file will show supply info

--- Food Truck Desktop/test/controller/TestController.java
+++ Food Truck Desktop/test/controller/TestController.java
@@ -2,7 +2,11 @@ package controller;
 
 import static org.junit.Assert.*;
 
+import java.awt.MenuItem;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -10,6 +14,8 @@ import org.junit.Test;
 
 import model.Equipment;
 import model.FTMS;
+import model.Staff;
+import model.Supply;
 import persistence.PersistenceXStream;
 
 public class TestController {
@@ -17,14 +23,16 @@ public class TestController {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		PersistenceXStream.setFilename("test" + File.separator + "controller" + File.separator + "data.xml");
+		PersistenceXStream.setAlias("staff", Staff.class);
 		PersistenceXStream.setAlias("equipment", Equipment.class);
 		PersistenceXStream.setAlias("FTMS", FTMS.class);
+		PersistenceXStream.setAlias("supply", Supply.class);
+		PersistenceXStream.setAlias("menuItem", MenuItem.class);
 	}
-
 
 	@After
 	public void tearDown() throws Exception {
-		// clear all equipments
+		// clear all
 		FTMS master = FTMS.getInstance();
 		master.delete();
 	}
@@ -33,11 +41,9 @@ public class TestController {
 	public void testCreateEquipment() {
 		FTMS master = FTMS.getInstance();
 		assertEquals(0, master.getEquipments().size());
-
 		String name = "Butter knife";
-		String description = "Careful it is sharp!!";
-		String quantity = "5";
-
+		String description = "Careful it is sharp!";
+		String quantity = "10";
 		Controller c = new Controller();
 		try {
 			c.createEquipment(name, description, quantity);
@@ -45,25 +51,19 @@ public class TestController {
 			// check that no error occurred
 			fail();
 		}
-
 		checkResultEquipment(name, description, quantity,  master);
-
 		FTMS master2 = (FTMS) PersistenceXStream.loadFromXMLwithXStream();
-
 		// check file contents
 		checkResultEquipment(name, description, quantity, master2);
-
 	}
 
 	@Test
 	public void testCreateEquipmentNull(){
 		FTMS master = FTMS.getInstance();
 		assertEquals(0, master.getEquipments().size());
-
 		String name = null;
 		String description = null;
 		String quantity = null;
-
 		String error = null;
 		Controller c = new Controller();
 		try {
@@ -71,10 +71,8 @@ public class TestController {
 		} catch(InvalidInputException e) {
 			error = e.getMessage();
 		}
-
 		// check error
 		assertEquals("Equipment name cannot be empty! Equipment quantity cannot be empty!", error);
-
 		// check no change in memory
 		assertEquals(0, master.getEquipments().size());
 	}
@@ -83,11 +81,9 @@ public class TestController {
 	public void TestCreateEquipmentEmpty(){
 		FTMS master = FTMS.getInstance();
 		assertEquals(0, master.getEquipments().size());
-
 		String name = "";
 		String description = "";
 		String quantity = "";
-
 		String error = null;
 		Controller c = new Controller();
 		try {
@@ -95,10 +91,8 @@ public class TestController {
 		} catch (InvalidInputException e) {
 			error = e.getMessage();
 		}
-
 		// check error
 		assertEquals("Equipment name cannot be empty! Equipment quantity cannot be empty!", error);
-
 		// check model in memory
 		assertEquals(0, master.getEquipments().size());
 	}
@@ -107,11 +101,9 @@ public class TestController {
 	public void testCreateEquipmentSpaces(){
 		FTMS master = FTMS.getInstance();
 		assertEquals(0, master.getEquipments().size());
-		
 		String name = " ";
 		String description = " ";
 		String quantity = " ";
-		
 		String error = null;
 		Controller c = new Controller();
 		try {
@@ -119,10 +111,8 @@ public class TestController {
 		} catch (InvalidInputException e) {
 			error = e.getMessage();
 		}
-		
 		// check error
 		assertEquals("Equipment name cannot be empty! Equipment quantity cannot be empty!", error);
-		
 		// check model in memory
 		assertEquals(0, master.getEquipments().size());
 	}
@@ -131,11 +121,9 @@ public class TestController {
 	public void testCreateEquipmentOutOfRange(){
 		FTMS master = FTMS.getInstance();
 		assertEquals(0, master.getEquipments().size());
-		
 		String name = "123456789012345678901234567890123456789012345678901";
 		String description = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012";
 		String quantity = "1000000000";
-		
 		String error = null;
 		Controller c = new Controller();
 		try {
@@ -143,10 +131,8 @@ public class TestController {
 		} catch (InvalidInputException e) {
 			error = e.getMessage();
 		}
-		
 		// check error
 		assertEquals("Equipment name cannot be too long! Equipment description cannot be too long! Equipment quantity cannot be too large!", error);
-		
 		// check model in memory
 		assertEquals(0, master.getEquipments().size());
 	}
@@ -155,11 +141,9 @@ public class TestController {
 	public void testCreateEquipmentQuanityNotInteger(){
 		FTMS master = FTMS.getInstance();
 		assertEquals(0, master.getEquipments().size());
-		
 		String name = "Butter knife";
 		String description = "Careful it is sharp!";
 		String quantity = "Hank";
-		
 		String error = null;
 		Controller c = new Controller();
 		try {
@@ -167,12 +151,258 @@ public class TestController {
 		} catch (InvalidInputException e) {
 			error = e.getMessage();
 		}
-		
 		// check error
 		assertEquals("Equipment quantity must be an integer!", error);
-		
 		// check model in memory
 		assertEquals(0, master.getEquipments().size());
+	}
+	
+	@Test
+	public void testCreateSupply() {
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getSupplies().size());
+		String name = "Tomato";
+		String description = "Do not use if rotten!";
+		String quantity = "10";
+		Controller c = new Controller();
+		try {
+			c.createSupply(name, description, quantity);
+		} catch (InvalidInputException e) {
+			// check that no error occurred
+			fail();
+		}
+		checkResultSupply(name, description, quantity,  master);
+		FTMS master2 = (FTMS) PersistenceXStream.loadFromXMLwithXStream();
+		// check file contents
+		checkResultSupply(name, description, quantity, master2);
+	}
+
+	@Test
+	public void testCreateSupplyNull(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getSupplies().size());
+		String name = null;
+		String description = null;
+		String quantity = null;
+		String error = null;
+		Controller c = new Controller();
+		try {
+			c.createSupply(name, description, quantity);
+		} catch(InvalidInputException e) {
+			error = e.getMessage();
+		}
+		// check error
+		assertEquals("Supply name cannot be empty! Supply quantity cannot be empty!", error);
+		// check no change in memory
+		assertEquals(0, master.getSupplies().size());
+	}
+
+	@Test
+	public void TestCreateSupplyEmpty(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getSupplies().size());
+		String name = "";
+		String description = "";
+		String quantity = "";
+		String error = null;
+		Controller c = new Controller();
+		try {
+			c.createSupply(name, description, quantity);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		// check error
+		assertEquals("Supply name cannot be empty! Supply quantity cannot be empty!", error);
+		// check model in memory
+		assertEquals(0, master.getSupplies().size());
+	}
+
+	@Test
+	public void testCreateSupplySpaces(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getSupplies().size());
+		String name = " ";
+		String description = " ";
+		String quantity = " ";
+		String error = null;
+		Controller c = new Controller();
+		try {
+			c.createSupply(name, description, quantity);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		// check error
+		assertEquals("Supply name cannot be empty! Supply quantity cannot be empty!", error);
+		// check model in memory
+		assertEquals(0, master.getSupplies().size());
+	}
+	
+	@Test
+	public void testCreateSupplyOutOfRange(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getSupplies().size());
+		String name = "123456789012345678901234567890123456789012345678901";
+		String description = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012";
+		String quantity = "1000000000";
+		String error = null;
+		Controller c = new Controller();
+		try {
+			c.createSupply(name, description, quantity);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		// check error
+		assertEquals("Supply name cannot be too long! Supply description cannot be too long! Supply quantity cannot be too large!", error);
+		// check model in memory
+		assertEquals(0, master.getSupplies().size());
+	}
+	
+	@Test
+	public void testCreateSupplyQuanityNotInteger(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getSupplies().size());
+		String name = "Tomato";
+		String description = "Do not use if rotten!";
+		String quantity = "Hank";
+		String error = null;
+		Controller c = new Controller();
+		try {
+			c.createSupply(name, description, quantity);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		// check error
+		assertEquals("Supply quantity must be an integer!", error);
+		// check model in memory
+		assertEquals(0, master.getSupplies().size());
+	}
+	
+	@Test
+	public void testCreateStaff() {
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getStaffs().size());
+		String name = "Hank";
+		String role = "Manager";
+		Controller c = new Controller();
+		try {
+			c.createStaff(name, role);
+		} catch (InvalidInputException e) {
+			// check that no error occurred
+			fail();
+		}
+		checkResultStaff(name, role,  master);
+		FTMS master2 = (FTMS) PersistenceXStream.loadFromXMLwithXStream();
+		// check file contents
+		checkResultStaff(name, role, master2);
+	}
+	
+	@Test
+	public void testCreateStaffNull(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getStaffs().size());
+		String name = null;
+		String role = null;
+		String error = null;
+		Controller c = new Controller();
+		try {
+			c.createStaff(name, role);
+		} catch(InvalidInputException e) {
+			error = e.getMessage();
+		}
+		// check error
+		assertEquals("Staff name cannot be empty! Staff role cannot be empty!", error);
+		// check no change in memory
+		assertEquals(0, master.getStaffs().size());
+	}
+	
+	@Test
+	public void TestCreateStaffEmpty(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getStaffs().size());
+		String name = "";
+		String role = "";
+		String error = null;
+		Controller c = new Controller();
+		try {
+			c.createStaff(name, role);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		// check error
+		assertEquals("Staff name cannot be empty! Staff role cannot be empty!", error);
+		// check model in memory
+		assertEquals(0, master.getStaffs().size());
+	}
+
+	@Test
+	public void testCreateStaffSpaces(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getStaffs().size());
+		String name = " ";
+		String role = " ";
+		String error = null;
+		Controller c = new Controller();
+		try {
+			c.createStaff(name, role);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		// check error
+		assertEquals("Staff name cannot be empty! Staff role cannot be empty!", error);
+		// check model in memory
+		assertEquals(0, master.getStaffs().size());
+	}
+	
+	@Test
+	public void testCreateStaffOutOfRange(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getStaffs().size());
+		String name = "123456789012345678901234567890123456789012345678901";
+		String role = "123456789012345678901234567890123456789012345678901";
+		String error = null;
+		Controller c = new Controller();
+		try {
+			c.createStaff(name, role);
+		} catch (InvalidInputException e) {
+			error = e.getMessage();
+		}
+		// check error
+		assertEquals("Staff name cannot be too long! Staff role cannot be too long!", error);
+		// check model in memory
+		assertEquals(0, master.getStaffs().size());
+	}
+	
+	@Test
+	public void testCreateMenuItem(){
+		FTMS master = FTMS.getInstance();
+		assertEquals(0, master.getMenuItems().size());
+		String name = "Tomato chips";
+		ArrayList<Supply> ingredients = new ArrayList<Supply>();
+		Controller c = new Controller();
+		try {
+			c.createSupply("Tomato", "hi", "10");
+		} catch (InvalidInputException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			c.createSupply("Potato", "hi", "20");
+		} catch (InvalidInputException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		ingredients.add(master.getSupply(0));
+		ingredients.add(master.getSupply(1));
+		try {
+			c.createMenuItem(name, ingredients);
+		} catch (InvalidInputException e) {
+			// check that no error occurred
+			fail();
+		}
+		checkResultMenuItem(name, ingredients, master);
+		FTMS master2 = (FTMS) PersistenceXStream.loadFromXMLwithXStream();
+		// check file contents
+		checkResultMenuItem(name, ingredients, master2);
 	}
 	
 	public void checkResultEquipment(String name, String description, String quantity, FTMS master){
@@ -182,4 +412,23 @@ public class TestController {
 		assertEquals(quantity, Integer.toString(master.getEquipment(0).getQuantity()));
 	}
 
+	public void checkResultSupply(String name, String description, String quantity, FTMS master){
+		assertEquals(1, master.getSupplies().size());
+		assertEquals(name, master.getSupply(0).getName());
+		assertEquals(description, master.getSupply(0).getDescription());
+		assertEquals(quantity, Integer.toString(master.getSupply(0).getQuantity()));
+	}
+	
+	public void checkResultStaff(String name, String role, FTMS master){
+		assertEquals(1, master.getStaffs().size());
+		assertEquals(name, master.getStaff(0).getName());
+		assertEquals(role, master.getStaff(0).getRole());
+	}
+	
+	public void checkResultMenuItem(String name, ArrayList<Supply> ingredients, FTMS master){
+		assertEquals(1, master.getMenuItems().size());
+		assertEquals(name, master.getMenuItem(0).getName());
+		assertEquals(ingredients.get(0).getName(), master.getMenuItem(0).getSupply(0).getName());
+		assertEquals(ingredients.get(1).getName(), master.getMenuItem(0).getSupply(1).getName());
+	}
 }
--- Food Truck Desktop/test/persistence/TestPersistence.java
+++ Food Truck Desktop/test/persistence/TestPersistence.java
@@ -3,6 +3,8 @@ package persistence;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,25 +13,51 @@ import org.junit.Test;
 import persistence.PersistenceXStream;
 import model.Equipment;
 import model.FTMS;
+import model.MenuItem;
+import model.Staff;
+import model.Supply;
 
 public class TestPersistence {
 
 	@Before
 	public void setUp() throws Exception {
 
-		// create FoodSupply
+		FTMS master = FTMS.getInstance();
+
+		// create Equipment
 		Equipment eq1 = new Equipment("Butter knife", "Careful it is sharp!", 5);
 		Equipment eq2 = new Equipment("Chef hat", "Do not wear it if you are not a chef!", 10);
 
-		// manage FoodSupply
-		FTMS master = FTMS.getInstance();
+		// create Supply
+		Supply s1 = new Supply("Tomato", "Do not use if rotten!", 10);
+		Supply s2 = new Supply("Potato", "Do not use if dark!", 20);
+		
+		// create Staff
+		Staff st1 = new Staff("Hank", "Manager");
+		Staff st2 = new Staff("Ali", "Engineer");
+
+		// create MenuItem
+		MenuItem mi1 = new MenuItem("Tomato chips");
+		mi1.addSupply(s1);
+		mi1.addSupply(s2);
+		MenuItem mi2 = new MenuItem("Potato ketchup");
+		mi2.addSupply(s2);
+		mi2.addSupply(s1);
+		
+		// manage 
 		master.addEquipment(eq1);
 		master.addEquipment(eq2);
+		master.addSupply(s1);
+		master.addSupply(s2);
+		master.addStaff(st1);
+		master.addStaff(st2);
+		master.addMenuItem(mi1);
+		master.addMenuItem(mi2);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		// clear all Equipments
+		// clear all
 		FTMS master = FTMS.getInstance();
 		master.delete();
 	}
@@ -41,19 +69,25 @@ public class TestPersistence {
 		PersistenceXStream.setFilename("test" + File.separator + "persistence" + File.separator + "data.xml");
 		PersistenceXStream.setAlias("equipment", Equipment.class);
 		PersistenceXStream.setAlias("FTMS", FTMS.class);
+		PersistenceXStream.setAlias("staff", Staff.class);
+		PersistenceXStream.setAlias("supply", Supply.class);
+		PersistenceXStream.setAlias("menuItem", MenuItem.class);
 		if(!PersistenceXStream.saveToXMLwithXStream(master))
 			fail("Could not save file.");
 
 		// clear the model in memory
 		master.delete();
 		assertEquals(0, master.getEquipments().size());
+		assertEquals(0, master.getSupplies().size());
+		assertEquals(0, master.getStaffs().size());
+		assertEquals(0, master.getMenuItems().size());
 
 		// load model
 		master = (FTMS) PersistenceXStream.loadFromXMLwithXStream();
 		if(master == null)
 			fail("Could not load file.");
 
-		// check food supplies
+		// check equipments
 		assertEquals(2, master.getEquipments().size());
 		assertEquals("Butter knife", master.getEquipment(0).getName());
 		assertEquals("Chef hat", master.getEquipment(1).getName());
@@ -63,6 +97,35 @@ public class TestPersistence {
 
 		assertEquals(5, master.getEquipment(0).getQuantity());
 		assertEquals(10, master.getEquipment(1).getQuantity());
+		
+		// check supplies
+		assertEquals(2, master.getSupplies().size());
+		assertEquals("Tomato", master.getSupply(0).getName());
+		assertEquals("Potato", master.getSupply(1).getName());
+		
+		assertEquals("Do not use if rotten!", master.getSupply(0).getDescription());
+		assertEquals("Do not use if dark!", master.getSupply(1).getDescription());
+		
+		assertEquals(10, master.getSupply(0).getQuantity());
+		assertEquals(20, master.getSupply(1).getQuantity());
+		
+		// check staffs
+		assertEquals(2, master.getStaffs().size());
+		assertEquals("Hank", master.getStaff(0).getName());
+		assertEquals("Ali", master.getStaff(1).getName());
+		
+		assertEquals("Manager", master.getStaff(0).getRole());
+		assertEquals("Engineer", master.getStaff(1).getRole());
+		
+		// check menu items
+		assertEquals(2, master.getMenuItems().size());
+		assertEquals("Tomato chips", master.getMenuItem(0).getName());
+		assertEquals("Potato ketchup", master.getMenuItem(1).getName());
+		
+		assertEquals(master.getSupply(0), master.getMenuItem(0).getSupply(0));
+		assertEquals(master.getSupply(1), master.getMenuItem(0).getSupply(1));
+		assertEquals(master.getSupply(1), master.getMenuItem(1).getSupply(0));
+		assertEquals(master.getSupply(0), master.getMenuItem(1).getSupply(1));
 	}
 
 }
