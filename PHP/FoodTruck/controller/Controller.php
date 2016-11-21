9c6e42b825e3fe925ee3c45720cddb85138c39ea
qingqing <dongwen.wang@gmail.com>
2016-11-21 17:10:56 -0500

    issue #21, create/add Suppply + test

--- PHP/FoodTruck/controller/Controller.php
+++ PHP/FoodTruck/controller/Controller.php
@@ -41,4 +41,38 @@ class Controller{
 			throw new Exception(trim($error));
 		} 
 	}
+	
+	public function createSupply($supply_name, $supply_quantity){
+		//1. Validate input
+		$error = "";
+		$name = InputValidator::validate_input($supply_name);
+		$quantity = InputValidator::validate_input($supply_quantity);
+		$isInt = preg_match('/^[0-9]+$/',$quantity);
+		if ($name!=null && strlen($name)!=0 && $quantity!=null && strlen($quantity)!=0 && $isInt){
+			//2. load all of the data
+			$pm = new PersistenceFoodTruck();
+			$rm = $pm->loadDataFromStore();
+	
+			//3. Add the new Equipment
+			$supply = new Supply($name,intval($quantity));
+			$rm->addSupply($supply);
+	
+			//4. Write all of the data
+			$pm->writeDataToStore($rm);
+		}
+		else {
+			if ($name == null || strlen($name)==0) {
+				$error .= "@1Supply name cannot be empty! ";
+			}
+			if ($quantity == null || strlen($quantity) ==0) {
+				$error .= "@2Supply quantity cannot be empty! ";
+			}
+			else if (!$isInt) {
+				$error .= "@2Supply quantity must be a positive integer! ";
+			}
+			throw new Exception(trim($error));
+		}
+	}
+	
+	
 }
--- PHP/FoodTruck/test/PersistenceFoodTruckTest.php
+++ PHP/FoodTruck/test/PersistenceFoodTruckTest.php
@@ -2,6 +2,8 @@
 require_once __DIR__.'/../persistence/PersistenceFoodTruck.php';
 require_once __DIR__.'/../model/FTMS.php';
 require_once __DIR__.'/../model/Equipment.php';
+require_once __DIR__.'/../model/Supply.php';
+
 
 class PersistenceFoodTruckManagement extends PHPUnit_Framework_TestCase
 {
@@ -17,8 +19,12 @@ class PersistenceFoodTruckManagement extends PHPUnit_Framework_TestCase
 	{
 		// 1. Create test data
 		$ftms = FTMS::getInstance();
+		//create equipment
 		$equipment = new Equipment("Pan",5);
 		$ftms->addEquipment($equipment);
+		//create supply
+		$supply = new Supply("Tomatoes",4);
+		$ftms->addSupply($supply);
 		
 		// 2. Write all of the data
 		$this->pm->writeDataToStore($ftms);
@@ -27,15 +33,23 @@ class PersistenceFoodTruckManagement extends PHPUnit_Framework_TestCase
 		$ftms->delete();
 		
 		$this->assertEquals(0,count($ftms->getEquipment()));
+		$this->assertEquals(0,count($ftms->getSupplies()));
+		
 		
 		//4. Load it back in 
 		$ftms = $this->pm->loadDataFromStore();
 		
 		//5. Check that we got it back
+		//check equipment
 		$this->assertEquals(1,count($ftms->getEquipment()));
 		$myEquipment = $ftms->getEquipment_index(0);
 		$this->assertEquals("Pan",$myEquipment->getName());
 		$this->assertEquals(5,$myEquipment->getQuantity());
+		//check supply
+		$this->assertEquals(1,count($ftms->getSupplies()));
+		$mySupply = $ftms->getSupply_index(0);
+		$this->assertEquals("Tomatoes",$mySupply->getName());
+		$this->assertEquals(4,$mySupply->getQuantity());
 		
 	}
 }
